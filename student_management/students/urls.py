16aa53584b2c52ad7527ccc00fbea386dfb7721d
Vishwesh <113243070+Salampuriavv@users.noreply.github.com>
2024-12-06 19:38:56 +0530

    Updated urls.py

--- student_management/students/urls.py
+++ student_management/students/urls.py
@@ -2,8 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.students_api, name='students'),  # for GET and POST of student taable
-    path('students/<int:id>/', views.students_api, name='student_detail'),  # for GET, PUT, DELETE of student table individual elements 
-    path('students_pno/', views.students_pno_api, name='students_pno'),  # for GET and POST of studentPno table 
-    path('students_pno/<int:pno_id>/', views.students_pno_api, name='students_pno_detail'),  # for PUT and DELETE  of studentPno table 
+    # Students table routes
+    path('students/', views.students_api, name='students'),  # for GET and POST of student table
+    path('students/<int:id>/', views.students_api, name='student_detail'),  # for GET, PUT, DELETE of student table individual elements
+
+    # StudentsPNo table routes
+    path('students_pno/', views.students_pno_api, name='students_pno'),  # for GET and POST of studentPno table
+    path('students_pno/<int:pno_id>/', views.students_pno_api, name='students_pno_detail'),  # for PUT and DELETE of studentPno table
+
+    # Subject table routes
+    path('subject/', views.subject_api, name='subject'),  # for GET and POST of subject table
+    path('subject/<int:id>/', views.subject_api, name='subject_detail'),  # for GET, PUT, PATCH, DELETE of subject table
+
+    # Scores table routes
+    path('scores/', views.scores_api, name='scores'),  # for GET and POST of scores table
+    path('scores/<int:id>/', views.scores_api, name='score_detail'),  # for GET, PUT, PATCH, DELETE of scores table
 ]
